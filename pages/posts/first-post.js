e6ae739282737fd607e99adfb11ae9a509abdae4
joy <1519105541@qq.com>
2022-01-19 21:53:44 +0800

    feat: 资源、元数据和 CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         {/* 该Link组件支持在同一个 Next.js 应用程序中的两个页面之间进行客户端导航。
@@ -13,8 +18,10 @@ export default function FirstPost() {
 
         {/* 如果您使用<a href="…">而不是<Link href="…">这样做，
         则在链接点击时背景颜色将被清除，因为浏览器会进行完全刷新 */}
-        <a href="/">Back to home</a>
+        <div>
+          <a href="/">Back to home</a>
+        </div>
       </h2>
-    </>
+    </Layout>
   );
 }
